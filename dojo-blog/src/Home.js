386b9ca9b81763a777cbeeb01e51828e8eb55c16
MarkEnnisDev <MarkEnnisDev@gmail.com>
2023-06-06 23:04:00 -0500

    l4m

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,20 @@
+import React, { useState } from 'react';
+
 const Home = () => {
-    let name = 'mario';
+    // let name = 'mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(24);
+
     const handleClick = (e) => {
-        console.log('hello, ninjas', e);
+        setName('luigi');
+        setAge(3000);
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
-    }
     return ( 
     <div className="home">
         <h2>Homepage</h2>
+        <p>{ name } is { age } years old</p>
         <button onClick={ handleClick }>Click me</button>
-        <button onClick={ (e) => handleClickAgain('mario', e) }>Click me again</button>
     </div> 
     );
 }
